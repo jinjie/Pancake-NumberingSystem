22ef236c01062a24f57957d66e0b94e6640736f3
Kong Jin Jie <jinjie@aspiredesigns.com.sg>
2013-07-17 21:36:11 +0800

    Comitting main source and markdown

---
+++ NumberingSystem/plugin.php
@@ -0,0 +1,118 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Numbering System
+ *
+ * A simple numbering system for PancakeApp. Introducing the new plugin system.
+ *
+ * @author		Kong Jin Jie
+ * @copyright	Copyright (c) 2013, Aspire Designs Pte Ltd
+ * @link		http://www.github.com
+ * @since		Version 1.0
+ */
+
+class Plugin_NumberingSystem extends Plugin {
+
+	/**
+	 * Some configuration for the plugin
+	 */
+	public $alias = 'numbering_system';
+	
+	public $name = array(
+		'en'	=> 'Numbering System',
+	);
+	
+	public $description = array(
+		'en'	=> 'A simple plugin to define your own numbering system',
+	);
+	
+	public $author = 'Aspire Designs Pte Ltd';
+	
+	public $url = 'http://www.aspiredesigns.com.sg/';
+	
+	/**
+	 * Configurables for the plugin and their default values
+	 */
+	 
+	public $config = array(
+		'fields' => array(
+			'invoice_pattern'	=> array(
+				'name'	=> 'invoice_pattern',
+				'label'	=> array(
+					'en'	=> 'Invoice Pattern',
+				),
+				'type'	=> 'text',
+				'default'	=> 'FA-{yyyy}{mm}{dd}-{num4}',
+			),
+		),
+	);
+	
+	
+	/**
+	 * Nothing interesting here...
+	 */
+	
+	private $ci;
+	
+	private $installed = FALSE;
+	
+	
+	public function __construct() {
+		$this->ci =& get_instance();
+		
+		$this->ci->load->model('plugins_m');
+		
+		$this->installed = $this->ci->plugins_m->get_plugin_setting($this->alias . '_installed');
+		
+		if ($this->installed) {
+			// Only register events when the plugin is installed
+			$this->register_events();
+		}
+	}
+	
+	private function register_events() {
+		/**
+		 * Register events to be hooked.
+		 * Add more if required. :)
+		 */
+		 
+		Events::register('invoice_number_generated', array($this, 'invoice_number_generated'));
+	}
+	
+	public function invoice_number_generated($count) {
+		/**
+		 * $count is passed by invoices_m which is the "real" invoice number
+		 */
+		 
+		if ($count === FALSE) {
+			// Looks like this is the first invoice, let's use invoice number 1
+			
+			$count = 1;
+		}
+		
+		// Variables we can use in the pattern
+		
+		$data = array(
+			'yyyy'		=> date('Y'),
+			'yy'		=> date('y'),
+			'dd'		=> date('d'),
+			'd'			=> date('j'),
+			'mm'		=> date('m'),
+			'mmm'		=> date('M'),
+			'num'		=> $count,
+			'num2'		=> str_pad($count, 2, '0', STR_PAD_LEFT),
+			'num3'		=> str_pad($count, 2, '0', STR_PAD_LEFT),
+			'num4'		=> str_pad($count, 2, '0', STR_PAD_LEFT),
+		);
+		
+		$this->ci->load->library('parser');
+		
+		// Return the custom invoice number back to invoices_m
+		
+		return $this->parser->parse_string($this->ci->plugins_m->get_plugin_setting('invoice_pattern'), $data, TRUE);
+	}
+
+}
+
+
+/* End of file */
--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 Pancake-NumberingSystem
 =======================
 
-A simple plugin initially developed to explore the new plugin system in PancakeApp
+A simple plugin initially developed to explore the new plugin system in PancakeApp.
+
+It's great news that PancakeApp have released a plugin system but no official document is out on how to develop plugins. So I have reverse engineered the codes and come out with this as an experiment. Feel free to add on to this project! :)
+
+Requirements
+------------
+
+You need an installed and running licensed PancakeApp v4 beta.
+
+Installation
+------------
+
+- Copy the folder, <code>NumberingSystem</code> to <code>./third_party/modules</code>
+- Login to PancakeApp and go to Plugins
+- Change the invoicing number pattern if required
+- Check "Enabled" to enable the plugin
+- Save Plugin Settings
+- Done!
+
+Try creating an invoice and you will see the new custom invoice number
+
+Variables
+---------
+
+{yyyy} - Year in YYYY, 2013
+{yy} - Year in YY, 13
+{dd} - Day in DD, 29
+{d} - Day in D, 9
+{mm} - Month in MM, 09
+{mmm} - Month in MMM, JAN / APR / DEC
+{num} - Original invoice number with no padding, 8
+{num2} - Original invoice number with 2 padding, 08
+{num2} - Original invoice number with 3 padding, 008
+{num2} - Original invoice number with 4 padding, 0008
+
+Developed by Kong Jin Jie @ [Aspire Designs Pte Ltd](http://www.aspiredesigns.com.sg)
